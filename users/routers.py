65b143fbedad86bb23b2727364682f96e124f27f
Svitlana Savikhina <savihina111@gmail.com>
2024-06-26 21:03:10 +0300

    Add user API endpoints for registration, login, and retrieval

---
+++ users/routers.py
@@ -0,0 +1,47 @@
+from datetime import timedelta
+from typing import List
+
+from fastapi import Depends, HTTPException, APIRouter
+from sqlalchemy.orm import Session
+
+from dependencies import get_db
+
+from users import schemas
+from users import crud
+from users.crud import create_access_token, ACCESS_TOKEN_EXPIRE_MINUTES
+
+router = APIRouter()
+
+
+@router.post("/register/", response_model=schemas.User)
+def register_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_username(db, username=user.username)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Username already registered")
+    return crud.create_user(db=db, user=user)
+
+
+@router.post("/login/", response_model=schemas.Token)
+def login_user(user: schemas.UserLogin, db: Session = Depends(get_db)):
+    db_user = crud.authenticate_user(db, username=user.username, password=user.password)
+    if not db_user:
+        raise HTTPException(status_code=401, detail="Incorrect username or password")
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": db_user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@router.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    user = crud.get_user_by_id(db, user_id=user_id)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+
+@router.get("/users/", response_model=List[schemas.User])
+def read_users(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    users = crud.get_all_users(db, skip=skip, limit=limit)
+    return users
