70b0b6f9acee3314c2b25ae0f8690c81d7cfe6b7
Svitlana Savikhina <savihina111@gmail.com>
2024-06-30 15:45:45 +0300

    edited users/routers.py using black

--- users/routers.py
+++ users/routers.py
@@ -5,10 +5,15 @@ from fastapi import Depends, HTTPException, APIRouter
 from sqlalchemy.orm import Session
 
 from dependencies import get_db
+from posts import models
 
 from users import schemas
 from users import crud
-from users.crud import create_access_token, ACCESS_TOKEN_EXPIRE_MINUTES
+from users.crud import (
+    create_access_token,
+    ACCESS_TOKEN_EXPIRE_MINUTES,
+    get_current_user,
+)
 
 router = APIRouter()
 
@@ -34,7 +39,11 @@ def login_user(user: schemas.UserLogin, db: Session = Depends(get_db)):
 
 
 @router.get("/users/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
+def read_user(
+    user_id: int,
+    db: Session = Depends(get_db),
+    current_user: models.User = Depends(get_current_user),
+):
     user = crud.get_user_by_id(db, user_id=user_id)
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
@@ -42,6 +51,11 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
 
 
 @router.get("/users/", response_model=List[schemas.User])
-def read_users(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+def read_users(
+    skip: int = 0,
+    limit: int = 10,
+    db: Session = Depends(get_db),
+    current_user: models.User = Depends(get_current_user),
+):
     users = crud.get_all_users(db, skip=skip, limit=limit)
     return users
